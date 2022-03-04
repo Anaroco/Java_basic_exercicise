a251c0864f5638e8f5bc2aff6a4526e6e17bd823
Anaroco <roblescortes@msn.com>
2022-03-04 20:00:43 +0100

    new repository

---
+++ demo/src/main/java/ejercicio1/ejercicio1.java
@@ -0,0 +1,44 @@
+package ejercicio1;
+
+import java.util.Scanner; // se busca en la API de Java 
+
+public class ejercicio1 {
+
+	 public static void main(String[] args) {
+		int a; 
+		//string b;
+		
+		Scanner S = new Scanner(System.in); //se crea un objeto
+		
+		System.out.println("Introduce tu edad por teclado:");
+		a = S.nextInt();
+		
+		System.out.println("Tu edad es " +a);
+		System.out.print("puedes pasar");
+	 
+	 }
+}
+
+
+
+
+
+
+
+// package com.ejercicio1;
+
+// /**
+//  * Hello world!
+//  */
+// public final class App {
+//     private App() {
+//     }
+
+//     /**
+//      * Says hello to the world.
+//      * @param args The arguments of the program.
+//      */
+//     public static void main(String[] args) {
+//         System.out.println("Hello World!");
+//     }
+// }
